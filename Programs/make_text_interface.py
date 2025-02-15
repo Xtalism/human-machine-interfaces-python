80496908e86af632c46794c232c2e365c35ce4d8
Xtalism <manuel_lego1234@hotmail.com>
2025-02-15 15:23:04 -0700

    code addition

--- Programs/make_text_interface.py
+++ Programs/make_text_interface.py
@@ -1,14 +1,24 @@
-import sys
+import sys, re
 from PyQt6 import uic
 from PyQt6.QtWidgets import *
+from ui_write_text import Ui_MainWindow
 
-class functionName(QMainWindow):
+class main(QMainWindow, Ui_MainWindow):
     def __init__(self): 
-        super(functionName, self).__init__()
-        uic.loadUi('', self)
-                
+        super(main, self).__init__()
+        uic.loadUi('write_text.ui', self)
+        self.buttonWrite.clicked.connect(self.writing)
+        
+    def writing(self):
+        filename = QFileDialog.getSaveFileName(self, caption='Save file',
+                                               filter='txt File(*.txt)')[0]
+        file = open(filename, 'w')
+        text = self.plainTextEdit.toPlainText()
+        file.write(text)
+        file.close()
+                                    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = functionName()
+    window = main()
     window.show()
     sys.exit(app.exec())
